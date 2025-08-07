ccdfccfea48b3dc48eecf1b58da12e403a3d21dd
romulo <romulo.inbox@gmail.com>
2025-08-06 21:31:54 -0300

    refactor: removed unused imports.

--- app/src/main/java/com/demo/airbnb/ui/features/login/LoginVM.kt
+++ app/src/main/java/com/demo/airbnb/ui/features/login/LoginVM.kt
@@ -1,6 +1,5 @@
 package com.demo.airbnb.ui.features.login
 
-import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.demo.airbnb.domain.entities.Session
@@ -10,7 +9,6 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
